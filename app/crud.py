420d7daa76975cdbeb7d65ef29b0833a67eba13a
eragon1248 <raksheetkota@gmail.com>
2023-08-12 23:46:13 -0500

    started ratings

--- app/crud.py
+++ app/crud.py
@@ -3,8 +3,8 @@ from sqlalchemy.orm import Session
 from . import models, schemas
 
 
-def get_user(db: Session, user_id: int):
-    return db.query(models.Users).filter(models.User.id == user_id).first()
+def get_user(db: Session, name: int):
+    return db.query(models.Users).filter(models.Users.name == name).first()
 
 def get_users(db: Session, skip: int = 0, limit: int = 100):
     return db.query(models.Users).offset(skip).limit(limit).all()
--- app/main.py
+++ app/main.py
@@ -19,34 +19,34 @@ def get_db():
     finally:
         db.close()
 
-
-@app.post("/users/", response_model=schemas.User)
+#add users
+@app.post("/users/add", response_model=schemas.User)
 def create_user(user: schemas.UserCreate, db: Session = Depends(get_db)):
     return crud.create_user(db=db, user=user)
 
-
+#read all users
 @app.get("/users/", response_model=List[schemas.User])
 def read_users(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     users = crud.get_users(db, skip=skip, limit=limit)
     return users
 
-
-@app.get("/users/{user_id}", response_model=schemas.User)
-def read_user(user_id: int, db: Session = Depends(get_db)):
-    db_user = crud.get_user(db, user_id=user_id)
+#read user data by name
+@app.get("/users/{name}", response_model=schemas.User)
+def read_user(name: int, db: Session = Depends(get_db)):
+    db_user = crud.get_user(db, name=name)
     if db_user is None:
         raise HTTPException(status_code=404, detail="User not found")
     return db_user
 
-
-@app.post("/users/{user_id}/items/", response_model=schemas.Rating)
+#post a rating from a user
+@app.post("/users/{user_id}/ratings/", response_model=schemas.Rating)
 def create_rating_for_user(
     user_id: int, item: schemas.RatingCreate, db: Session = Depends(get_db)
 ):
     return crud.create_user_rating(db=db, item=item, user_id=user_id)
 
-
-@app.get("/items/", response_model=List[schemas.Rating])
+#read ratings by restaurant
+@app.get("/restaurant/{restaurant}/ratings/", response_model=List[schemas.Rating])
 def read_ratings(skip: int = 0, limit: int = 100, db: Session = Depends(get_db)):
     items = crud.get_ratings(db, skip=skip, limit=limit)
     return items
