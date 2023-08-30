10ebea55bcfccf3812ae31dc124c4594c41a6d57
eragon1248 <raksheetkota@gmail.com>
2023-08-29 20:37:55 -0500

    added testing dependency which tests the user function

--- tests/test_db.py
+++ tests/test_db.py
@@ -1,11 +1,59 @@
 from fastapi.testclient import TestClient
-from app.main import app
-from app import schemas
+from sqlalchemy import create_engine
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.pool import StaticPool
 
+from app.database import Base
+from app.main import app, get_db
+
+import os
+from dotenv import load_dotenv
+
+load_dotenv()
+
+host     = os.getenv('HOST')
+user     = os.getenv('DBUSER')
+password = os.getenv('PASSWORD')
+database = os.getenv('DATABASE')
+port     = os.getenv('PORT')
+
+SQLALCHEMY_DATABASE_URL = f"postgresql://{user}:{password}@{host}:{port}/{database}"
+
+engine = create_engine(SQLALCHEMY_DATABASE_URL)
+TestingSessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+
+Base.metadata.create_all(bind=engine)
+
+
+def override_get_db():
+    db = None
+    try:
+        db = TestingSessionLocal()
+        yield db
+    finally:
+        if db:
+            db.close()
+
+
+app.dependency_overrides[get_db] = override_get_db
 
 client = TestClient(app)
 
-def test_read_users():
-    response = client.get("/users/")
-    assert response.status_code == 200
-    print(response.json())
+
+def test_create_user():
+    response = client.post(
+        "/users/add",
+        json={"name": "abhinav", "password": "chimichangas"},
+    )
+    assert response.status_code == 200, response.text
+    data = response.json()
+    assert "name" in data
+    assert data["name"] == "abhinav"
+    user_name = data["name"]
+
+    response = client.get(f"/users/name/{user_name}")
+    assert response.status_code == 200, response.text
+    data = response.json()
+    assert data["name"] == "abhinav"
+    assert data["name"] == user_name
