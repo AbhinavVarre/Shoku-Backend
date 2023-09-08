5cac232bc9a4c40f01f5d4ebdb16fb2504b153ca
eragon1248 <raksheetkota@gmail.com>
2023-09-08 13:51:03 -0500

    got parameters for tag operations

--- app/routers/tags.py
+++ app/routers/tags.py
@@ -12,21 +12,21 @@ router = APIRouter(
 
 
 #Create a tag
-@router.post("/{owner_name}/add", response_model=schemas.RestaurantList, summary="create a tag")
-def create_tag(list: schemas.RestaurantListCreate, owner_name: str, db: Session = Depends(get_db)):
+@router.post("/add", response_model=schemas.Tag, summary="create a tag")
+def create_tag(tag: schemas.Tag, db: Session = Depends(get_db)):
     pass
 
 #Tag a restaurant
-@router.post("/{owner_name}/{list_name}/add/{restaurant_name}", response_model=schemas.RestaurantList, summary="Tag a restaurant")
-def tag_restaurant(owner_name: str, list_name: str, restaurant_name:str, db: Session = Depends(get_db)):
+@router.post("/{restaurant_name}/add/{tag_name}", response_model=schemas.Restaurant, summary="Tag a restaurant")
+def tag_restaurant(restaurant_name: str, tag_name, db: Session = Depends(get_db)):
     pass
 
 #Read all restaurants assocaited with a tag
-@router.get("/{owner_name}/all", response_model=list[schemas.Restaurant],summary="Read all restaurants assocaited with a tag")
-def get_restaurants_by_tag(owner_name: str, db: Session = Depends(get_db)):
+@router.get("/{tag_name}/restaurants", response_model=list[schemas.Restaurant],summary="Read all restaurants assocaited with a tag")
+def get_restaurants_by_tag(tag_name: str, db: Session = Depends(get_db)):
     pass
 
 #Read all tags associated with a restaurant
-@router.get("/{owner_name}/{list_name}", response_model=list[schemas.Tag], summary="Read all tags associated with a restaurant")
-def get_tags_by_restaurant(owner_name: str, list_name: str, db: Session = Depends(get_db)):
+@router.get("/{restaurant_name}/tags", response_model=list[schemas.Tag], summary="Read all tags associated with a restaurant")
+def get_tags_by_restaurant(restaurant_name: str, db: Session = Depends(get_db)):
     pass
